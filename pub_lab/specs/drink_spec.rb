aa362dbc14ab48161dc30f055a10f186543c3805
Annabel Treshansky <annabelt@Annabels-MacBook-Air.local>
2019-03-06 11:35:35 +0000

    drinks tests

--- pub_lab/specs/drink_spec.rb
+++ pub_lab/specs/drink_spec.rb
@@ -14,4 +14,11 @@ def test_drink_name
   assert_equal("Bucky", @drink1.name)
 end
 
+def test_drink_price
+  assert_equal(7, @drink1.price)
+end
+def test_drink_alcohol_level
+  assert_equal(3, @drink1.alcohol_level)
+end
+
 end  # end class
